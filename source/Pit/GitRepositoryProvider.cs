68c6b2d55a4d0713f716fc2646ee21cbd5f27050
Manoj Mahalingam <manojlds@gmail.com>
2012-12-01 23:26:33 +0530

    Adding the Git provider with a default drive

---
+++ source/Pit/GitRepositoryProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Management.Automation;
+using System.Management.Automation.Provider;
+
+namespace Pit
+{
+    [CmdletProvider("Git", ProviderCapabilities.None)]
+    public class GitRepositoryProvider : ItemCmdletProvider
+    {
+        protected override Collection<PSDriveInfo> InitializeDefaultDrives()
+        {
+            return new Collection<PSDriveInfo>
+                       {
+                           new PSDriveInfo("Git", ProviderInfo, "git:", "Git Provider", null, true)
+                       };
+        }
+
+        protected override bool IsValidPath(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
