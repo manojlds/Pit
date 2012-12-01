4b987ff5b6e5d3b9d35641620bd016e5c17eadf7
Manoj Mahalingam <manojlds@gmail.com>
2012-12-02 00:07:27 +0530

    Adding a custom drive info object
    Holds the config file path which stores the tracked repos

---
+++ source/Pit/GitDriveInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace Pit
+{
+    internal class GitDriveInfo : PSDriveInfo
+    {
+        public string GitDriveConfigFile { get; private set; }
+
+        public GitDriveInfo(string name, ProviderInfo provider, string root, string description, PSCredential credential, bool persist) : base(name, provider, root, description, credential, persist)
+        {
+            GitDriveConfigFile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        }
+    }
+}
--- source/Pit/GitRepositoryProvider.cs
+++ source/Pit/GitRepositoryProvider.cs
@@ -8,17 +8,44 @@ namespace Pit
     [CmdletProvider("Git", ProviderCapabilities.None)]
     public class GitRepositoryProvider : ContainerCmdletProvider
     {
+        private const string PathSeparator = @"\";
+
         protected override Collection<PSDriveInfo> InitializeDefaultDrives()
         {
             return new Collection<PSDriveInfo>
                        {
-                           new PSDriveInfo("Git", ProviderInfo, "git:", "Git Provider", null, true)
+                           new GitDriveInfo("Git", ProviderInfo, "git:", "Git Provider", null, true)
                        };
+        }
+
+        protected override void GetItem(string path)
+        {
+            if (PathIsDrive(path))
+            {
+                WriteItemObject(PSDriveInfo, path, true);
+                return;
+            }
+        }
+
+        protected override bool ItemExists(string path)
+        {
+            if (PathIsDrive(path))
+            {
+                return true;
+            }
+
+            return false;
         }
 
         protected override bool IsValidPath(string path)
         {
             throw new NotImplementedException();
+        }
+
+        private bool PathIsDrive(string path)
+        {
+            return String.IsNullOrEmpty(path.Replace(PSDriveInfo.Root, string.Empty)) ||
+                   String.IsNullOrEmpty(path.Replace(PSDriveInfo.Root + PathSeparator, string.Empty));
         }
     }
 }
