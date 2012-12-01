844869cb5c08f2c8238e86bac06cdc06557f3890
Manoj Mahalingam <manojlds@gmail.com>
2012-12-01 22:22:38 +0530

    Adding the Pit snapin

---
+++ source/Pit/Pit.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Management.Automation;
+
+namespace Pit
+{
+    [RunInstaller(true)]
+    public class Pit : PSSnapIn
+    {
+        public override string Name
+        {
+            get { return "Pit"; }
+        }
+
+        public override string Vendor
+        {
+            get { return "StackToHeap"; }
+        }
+
+        public override string Description
+        {
+            get { return "Git provider for Powershell"; }
+        }
+    }
+}
