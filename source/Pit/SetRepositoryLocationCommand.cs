5b5a8108bea1afdcf5fabcd8135913e45e51942f
Manoj Mahalingam <manojlds@gmail.com>
2012-12-08 02:21:08 +0530

    Adding a cmdlet to cd into a repo from proovider path

---
+++ source/Pit/SetRepositoryLocationCommand.cs
@@ -0,0 +1,28 @@
+﻿using System.Management.Automation;
+using Pit.Exceptions;
+using Pit.GitDriveConfig;
+
+namespace Pit
+{
+    [Cmdlet(VerbsCommon.Set, "RepoLocation")]
+    public class SetRepositoryLocationCommand : PSCmdlet
+    {
+        private readonly IGitConfigManager gitConfigManager;
+
+        public SetRepositoryLocationCommand()
+        {
+            gitConfigManager = new GitConfigManager();
+        }
+
+        [Parameter(Mandatory = true)]
+        public string Name { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            var trackedRepository = gitConfigManager.GetTrackedRepository(Name);
+            if (trackedRepository == null) throw new RepositoryNotTrackedException(Name);
+
+            SessionState.Path.SetLocation(trackedRepository.Path);
+        }
+    }
+}
